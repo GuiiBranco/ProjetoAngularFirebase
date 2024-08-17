ec716bcc463a337ae01d7542a2bf8058ada370af
Guilherme Branco Rodrigues <151711519+GuiiBranco@users.noreply.github.com>
2024-08-17 01:48:34 +0000

    Academia Incrível

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,21 +10,77 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  perfil = {
-    foto: '',
-    nome: '',
-    profissao: '',
-    email: '',
-    idioma: '',
-    cidade_estado: '',
-    bio: '',
-    metrica: []
+
+  segments: string = 'usuarios';
+
+  usuarios = [
+    {
+      nome: 'Leôncio',
+      idade: 20,
+      genero: 'Masculino',
+      rotinas: [
+        {
+          dia: 'Segunda-Feira',
+          treinos: [
+            "Rosca Direta",
+            "Crucifixo",
+            "Remada Sumô",
+            "Bolinha de Golfe"
+          ]
+        },
+        {
+          dia: 'Quarta-Feira',
+          treinos: [
+            "Rosca Invertida",
+            "Pulley",
+            "Remada Sentada"
+          ]
+        },
+        {
+          dia: 'Sexta-Feira',
+          treinos: [
+            "Supino Inclinado",
+            "Bíceps Corda",
+            "Bíceps Hulk Invertido"
+          ]
+        }
+      ]
+    },
+    {
+      nome: 'Max-Steel',
+      idade: 28,
+      genero: 'Masculino',
+      rotinas: [
+        {
+          dia: 'Terça-Feira',
+          treinos: [
+            "Modo Turbo",
+            "Mil Vezes Mais",
+            "FORTE"
+          ]
+        },
+        {
+          dia: 'Quinta-Feira',
+          treinos: [
+            "Supi... Alpino",
+            "Peso de 200kg",
+            "Remada no Bote"
+          ]
+        },
+        {
+          dia: 'Sábado',
+          treinos: [
+            "Pular o Leg-Day",
+            "1000 Pulos de Corda",
+            "200 Voltas no Quarteirão"
+          ]
+        }
+      ]
+    }
+  ]
+
+  trocar(event: any) {
+    this.segments = event.detail.value;
   }
-  
-  amigo = {
-    foto: '',
-    nome: '',
-    mencao: '',
-    msg: ''
-  }
+
 }
